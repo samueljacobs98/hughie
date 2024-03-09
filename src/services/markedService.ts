8cc206687e76816f20a415a4428bf152c97f166e
Samuel Jacobs <sammyjacobscontact@gmail.com>
2024-03-09 16:39:40 +0000

    updated p tags

--- src/services/markedService.ts
+++ src/services/markedService.ts
@@ -2,8 +2,12 @@ import { marked } from "marked";
 
 const renderer = new marked.Renderer();
 
+renderer.paragraph = (text) => {
+  return `<p class="my-2">${text}</p>`;
+};
+
 renderer.code = (code, language) => {
-  return `<pre class="bg-gray-100 p-4 rounded-md"><code class="language-${language} text-gray-800">${code}</code></pre>`;
+  return `<pre class="bg-gray-800 p-4 rounded-md"><code class="language-${language} text-gray-100">${code}</code></pre>`;
 };
 
 marked.use({ renderer });
